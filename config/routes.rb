91a04ca5d8a55d51bdc066cea0a5bb59b15f2de5
Andy Feidje Joseph <ingandy75@gmail.com>
2019-08-05 17:34:13 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts , only: [:index, :show, :new, :create]
+  resources :posts , only: [:index, :show, :new, :create, :edit, :update]
 
-  put 'posts/:id', to: 'posts#update'
-  get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+  # put 'posts/:id', to: 'posts#update'
+  # get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
   # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
 
   # patch 'articles/:id', to: 'articles#update'
